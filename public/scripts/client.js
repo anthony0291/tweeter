fb83e0e8f95d0e5285ddf9213ea2f5ff545c1bb7
bzrkrbddy081 <bzrkrbddy081@gmail.com>
2023-06-23 19:08:31 +0000

    got tweet submit to work

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,230 +4,114 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-  const createTweetElement = (tweetData) => {
-    const { name, handle } = tweetData["user"];
-    const { text } = tweetData["content"];
-    const avatar = tweetData['user']['avatars'];
+const createTweetElement = (tweetData) => {
+  const { name, handle } = tweetData["user"];
+  const { text } = tweetData["content"];
+  const avatar = tweetData['user']['avatars'];
   
-    const $tweet = $("<article>");
-    const $title = $("<h3>");
-    const $content = $("<p>");
-    const $author = $("<p>");
+  const $tweet = $("<article>");
+  const $title = $("<h3>");
+  const $content = $("<p>");
+  const $author = $("<p>");
 
-    const $avatar = $(`<img src= ${avatar}>`);
-    const $symbols = $('<div class="symbols"><i class="fa-solid fa-flag"></i><i class="fa-solid fa-retweet"></i><i class="fa-solid fa-heart"></i></div>');
-    const $timeAgo = $(`<div>${$.timeago(tweetData['created_at'])}</div>`);
+  const $avatar = $(`<img src= ${avatar}>`);
+  const $symbols = $('<div class="symbols"><i class="fa-solid fa-flag"></i><i class="fa-solid fa-retweet"></i><i class="fa-solid fa-heart"></i></div>');
+  const $timeAgo = $(`<div>${$.timeago(tweetData['created_at'])}</div>`);
     
-
-    $title.text(name);
-    $content.text(text);
-    $author.text(handle);
-    // $timeAgo.text($timeAgo);
-    // $avatar.text($avatar);
-    // $symbols.text($symbols);
-
-    $tweet.append($title);
-    $tweet.append($avatar);
-    $tweet.append($author);
-    $tweet.append($content);
-    $tweet.append($timeAgo);
-    $tweet.append($symbols);
+  $title.text(name);
+  $content.text(text);
+  $author.text(handle);
+  // $timeAgo.text($timeAgo);
+  // $avatar.text($avatar);
+  // $symbols.text($symbols);
+  $tweet.append($title);
+  $tweet.append($avatar);
+  $tweet.append($author);
+  $tweet.append($content);
+  $tweet.append($timeAgo);
+  $tweet.append($symbols);
   
-    console.log($.timeago(tweetData['created_at']));
-    // console.log(tweetData['created_at']);
-    return $tweet;
-  };
+  console.log($.timeago(tweetData['created_at']));
+  // console.log(tweetData['created_at']);
+  return $tweet;
+};
 
-  const addTweetToPage = (target, post) => {
-    $(target).append(post);
-  };
+const addTweetToPage = (target, post) => {
+  $(target).append(post);
+};
+const renderTweets = (target, postList) => {
+  for (const i of postList) {
+    const newPost = createTweetElement(i);
+    addTweetToPage(target, newPost);
+  }
+};
+const loadTweets = function() {
+  $.ajax('/tweets', { method: "GET" }).then((res) => {
+    renderTweets("#tweets-container", res);
+  });
+};
 
-  const renderTweets = (target, postList) => {
-    for (const i of postList) {
-      const newPost = createTweetElement(i);
-      addTweetToPage(target, newPost);
-    }
-  };
 
+
+// const myProfile = function(textarea) {
+//   console.log('myprofile');
+//   let newData = [
+//     {
+//       "user": { "name": "Anthony Tran",
+//         "avatars": "https://i.imgur.com/73hZDYK.png",
+//         "handle": "@anthonytran0291"},
+        
+//       "content":{ "text": `${textarea}` },
+        
+//       "created_at":  Date.now(),
+//     }
+//   ];
+//   return newData;
+// };
 
 $(document).ready(function() {
+  loadTweets();
 
-  $('.new-tweet tweet').submit(function(event) {
-    event.preventDefault();
-    if ($.ajax({
+  const sendPostToBackend = (textarea) => {
+    console.log("top of sendPosttoBackEnd");
+    $.ajax("/tweets", {
       method: "POST",
-      url:"/tweets/",
-      data: $this.serialize(),
-    })) {
-      (goodPost) => {
-        $.ajax('/tweets/', { method: "GET" }).then((res) => {
-          renderTweets("#tweets-container", res);
-        });
-      };
-    }
-  });
-
-
-  const loadTweets = function() {
-    $.ajax('/tweets/', { method: "GET" }).then((res) => {
+      data: textarea,
+      // data: $this.serialize(),
+    }).then((res) => {
+      // const newPost = createTweetElement(res);
+      console.log("afternewPost");
       renderTweets("#tweets-container", res);
+      // const newPost = createTweetElement(myProfile(res.data));
+      // console.log(myProfile);
+      console.log("tweetrendered");
     });
   };
 
-  loadTweets();
+  $("form").on("submit", function(event) {
+    event.preventDefault();
+    // console.log("hello", $(event.currentTarget).serialize());
+    // const text = $('textarea').val();
+    const text = $(this).serialize();
+    console.log(`Form submiss =  ${text}`);
+    sendPostToBackend(text);
+  });
+  
 
+
+
+
+// console.log( $( this ).serialize() );
+
+// $( "form" ).on( "submit", function( event ) {
+//   event.preventDefault();
+//   console.log( $( this ).serialize() );
+// });
+
+
+
+
+
+///end of .document
 });
-
-
-
-
-
-
-
-// const tweetData = [
-//   {
-//     "user": {
-//       "name": "Newton",
-//       "avatars": "https://i.imgur.com/73hZDYK.png",
-//       "handle": "@SirIsaac"
-//     },
-//     "content": {
-//       "text": "If I have seen further it is by standing on the shoulders of giants"
-//     },
-//     "created_at": 1461116232227
-//   }];
-
-
-
-// const tweetData = [
-//   {
-//     "user": {
-//       "name": "Newton",
-//       "avatars": "https://i.imgur.com/73hZDYK.png",
-//       "handle": "@SirIsaac"
-//     },
-//     "content": {
-//       "text": "If I have seen further it is by standing on the shoulders of giants"
-//     },
-//     "created_at": 1461116232227
-//   }];
-
-
-
-
-
-
-//call renderTweets(#tw,tD) which calls
-//createTweetElement()
-
-// const localDataPost2 = createTweetElement(tweetData[0]);
-
-// renderTweets("#tweets-container", tweetData);
-
-
-
-
-// $.ajax({
-//   method:"GET",
-//   url: "http:localhost:8080/api/posts",
-// });
-
-
-
-//   $.ajax({
-//     method: "GET",
-//     url: "http://localhost:3000/api/posts",
-//     // url: "https://jsonplaceholder.typicode.com/posts",
-//   }).then((res) => {
-//     addAllPostsToPage("#post-container-4", res);
-//   });
-
-
-// $('.new-tweet p').empty().slideUp();
-// if ($.ajax({ url: "/tweets/", method: 'POST', data: $this.serialize() })) {
-//      (successfulPost) => $.ajax('/tweets/', { method: 'GET'});
-//  };
-
-
-// const createTweetElement = (tweetData) => {
-//   const $tweet = $article;
-//   const { name, avatars, handle } = tweetData["user"];
-//   const { text } = tweetData["content"];
-
-//   const $article = $("<article>");
-//   const $title = $("<h3 class ='top'>");
-//   const $content = $("<p class='top'>");
-//   const $author = $("<p class='top'>");
-//   const $avatars = $(`<img class='top' src=${tweetData['user']['avatars']}>`);
-//   const $symbols = $('<div class="symbols"><i class="fa-solid fa-flag"></i><i class="fa-solid fa-retweet"></i><i class="fa-solid fa-heart"></i></div>');
-
-//   $title.text(name);
-//   $content.text(text);
-//   $author.text(handle);
-
-//   $article.append($title);
-//   $article.append($avatars);
-//   $article.append($avatars);
-//   $article.append($author);
-//   $article.append($content);
-//   $article.append($symbols);
-  
-//   return $tweet;
-  
-// };
-
-// // const $tweetie = $(`<article class="tweet">Hello world</article>`);
-// const $tweet = createTweetElement(tweetData);
-// // const localDataPost = createTweetElement(tweetData[0]);
-
-
-
-
-// // Test / driver code (temporary)
-// // console.log($tweetie); // to see what it looks like
-// $('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
-// // addTweetToPage("#tweets-container", localDataPost);
-// // renderTweets('#tweets-container', tweetData);
-
-
-// // $(main.container .button).on("click", (event) => {
-// //   console.log("click");
-// // });
-
-// //ready
-// });
-
-// });
-
-//  const created = tweetData["created_at"];
-//   // const $avatars = $("<p>");
-//   const $footer = $("span class='created'>" + created + "</span>");
-//   const $symbols = ('<div class="symbols"><i class="fa-solid fa-flag"></i><i class="fa-solid fa-retweet"></i><i class="fa-solid fa-heart"></i></div>');
-
-//   $title.text(name);
-//   $content.text(text);
-//   $author.text(handle);
-//   // $created.text(tweetData["created_at"]);
-//   // $avatars(avatars);
-  
-
-//   $article.append($title);
-//   $article.append(avatars);
-//   $article.append($author);
-//   $article.append($content);
-//   //$article.append($created);
-//   $article.append($symbols);
-//   $article.append($footer);
-  
-// const addPostToPage = (target, post) => {
-//   $(target).append(post);
-// };
-  
-//   for (const tweets of tweetData) {
-//     const newPost =
-//   }
-//   const $tweet = $()
-//   // timestamp: 1:05:12
-// }
-    
-//add all tweets to pag
+///
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,27 +2,24 @@
 $(document).ready(function() {
   // --- our code goes here ---
 
-  console.log(this);
-  console.log("it's working!");
-  
-  $('.new-tweet textarea').on('input', function() {
-    let newTweetLength = $(this).val().length;
-    let nearbyCounter = $(this).siblings('.counter');
-    const tweetLengthLimit = 140;
-    console.log(".new-tweet:active");
-    
-    if (newTweetLength > tweetLengthLimit) {
-      nearbyCounter.addClass('tweetIsTooLong');
-    } else if (newTweetLength < tweetLengthLimit) {
-      nearbyCounter.removeClass('tweetIsTooLong');
-    }
-    nearbyCounter.text(tweetLengthLimit - newTweetLength);
+  // console.log(this);
+  // console.log("it's working!");
 
-    $("#btn").on('click', function() {
-      console.log(this); //The this keyword is a reference to the button
-    });
+  $('.new-tweet textarea').on('input', function() {
+    let tweetLength = $(this).val().length;
+    let tweetCounter = $(this).siblings('.counter');
+    const tweetLimit = 140;
+
+    if (tweetLength > tweetLimit) {
+      tweetCounter.addClass('tweetIsTooLong');
+    } else if (tweetLength < tweetLimit) {
+      tweetCounter.removeClass('tweetIsTooLong');
+    }
+    tweetCounter.text(tweetLimit - tweetLength);
     
   });
 
 
 });
+
+
