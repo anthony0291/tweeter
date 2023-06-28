f5e0eacd901577c9f351f61f76ddac12cc89c0e9
bzrkrbddy081 <bzrkrbddy081@gmail.com>
2023-06-28 06:09:08 +0000

    fixed based on feedback

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,11 +6,11 @@
 
 const createTweetElement = (tweetData) => {
   const { name, handle } = tweetData["user"];
-  const { text } = tweetData["content"];  //tweet text
-  const avatar = tweetData['user']['avatars']; //image link
+  const { text } = tweetData["content"];
+  const avatar = tweetData['user']['avatars'];
   
-  const $tweet = $("<article>")
-  const $tweetTop = $("<div class='top'>"); //main container
+  const $tweet = $("<article>");
+  const $tweetTop = $("<div class='top'>");
   const $tweetMid = $("<div class='mid'>");
   const $tweetBot = $("<div class='bot'>");
   
@@ -23,19 +23,10 @@ const createTweetElement = (tweetData) => {
   const $timeAgo = $(`<em>${$.timeago(tweetData['created_at'])}</em>`);
   const $symbols = $('<em class="symbols"><i class="fa-solid fa-flag"></i><i class="fa-solid fa-retweet"></i><i class="fa-solid fa-heart"></i></em>');
 
-// avatar/title/author   Top
-//Text                   Middle
-// days/ symbols         Bottom
-
   $title.text(name);
   $content.text(text);
   $author.text(handle);
-  // $timeAgo.text($timeAgo);
-  // $avatar.text($avatar);
-  // $symbols.text($symbols);
-  
 
-  //Appending
   $tweet.append($tweetTop.append($avatar));
   $tweet.append($tweetTop.append($title));
   $tweet.append($tweetTop.append($author));
@@ -43,7 +34,7 @@ const createTweetElement = (tweetData) => {
   $tweet.append($tweetBot.append($timeAgo));
   $tweet.append($tweetBot.append($symbols));
   console.log($.timeago(tweetData['created_at']));
-  // console.log(tweetData['created_at']);
+
   return $tweet;
 };
 
@@ -88,14 +79,14 @@ const sendPostToBackend = (textarea) => {
       }, 100);
     });
 
-    // alert('Error: Tweet is too Tweety!');
-
   } else {
     $.ajax("/tweets", { method: "POST", data: textarea }).then((res) => {
       renderTweets("#tweets-container", res);
       console.log("tweetRendered");
       setTimeout(function() {
         loadTweets(textarea);
+        $('textarea').val("");
+        $('.counter').text(140);
       }, 5);
     });
   }
@@ -111,35 +102,7 @@ $(document).ready(function() {
     event.preventDefault();
     sendPostToBackend($(this).serialize());
   });
-  
-
-
-
-
-
 
 ///////// END OF DOCUMENT
 });
-/////////
 
-
-
-
-
-
-
-// const myProfile = function(textarea) {
-//   console.log('myprofile');
-//   let newData = [
-//     {
-//       "user": { "name": "Anthony Tran",
-//         "avatars": "https://i.imgur.com/73hZDYK.png",
-//         "handle": "@anthonytran0291"},
-        
-//       "content":{ "text": `${textarea}` },
-        
-//       "created_at":  Date.now(),
-//     }
-//   ];
-//   return newData;
-// };
